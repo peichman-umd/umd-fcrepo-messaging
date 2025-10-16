4cde5333a05a052b54548a70d481ba5e0fbfeb15
Peter Eichman <peichman@umd.edu>
2025-10-16 14:37:57 -0400

    LIBFCREPO-1708. Updates for easier readability.
    - removed nested conditionals and rely on a "check condition and exit early" strategy
    - created a `log` function to encapsulate either sending to STDOUT or to the pod log and STDERR based on the log severity
    - check for at least 1 argument and exit with usage information if not present
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1708

--- amistuck.sh
+++ amistuck.sh
@@ -13,6 +13,12 @@
 # time the size was recorded as changing. If that value is greater than the
 # threshold, the script emits an error message and exits with a nonzero status.
 set -euo pipefail
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 QUEUE_NAME [TIMEOUT]"
+    exit 1
+fi
+
 QUEUE=$1
 THRESHOLD=${2:-300}
 
@@ -39,35 +45,56 @@ check_writable() {
 }
 check_writable "${SIZE_FILE}"
 
-last_size=$(cat "$SIZE_FILE" 2>/dev/null)
+log() {
+    local severity=$1
+    local payload=$2
+    local log_line="severity=${severity} ${payload}"
+    if [ "$severity" == "warning" ] || [ "$severity" == "error" ]; then
+        # for warnings and errors, send the log message to both:
+        # the pod log (file descriptor 1 of pid 1)...
+        echo "${log_line}"> /proc/1/fd/1
+        # ...and the liveness probe failure message (STDERR)
+        echo "${log_line}" >&2
+    else
+        # all other log levels just go to STDOUT
+        echo "${log_line}"
+    fi
+}
+
 current_size=$(curl -s -H 'Origin: http://localhost' "$URL" | grep -o -e '"QueueSize":[0-9]*' | cut -d: -f2)
 
-if [ -z "$last_size" ]; then
-  # no size recorded yet; record the size and exit success
-  echo "$current_size" > "$SIZE_FILE"
-  exit 0
-else
-  if (( current_size != last_size )); then
-    # size changed; record the new size and exit success
+# update state and exit early if there is no previous value to compare with
+if [ ! -e "$SIZE_FILE" ]; then
+    log info "queue=\"${QUEUE}\" msg=\"no previous size recorded, current size will be recorded\" elapsed=\"n/a\" last_size=\"n/a\" current_size=\"${current_size}\""
+    # only when it is changed do we update the size file (to preserve the timestamp of the change)
     echo "$current_size" > "$SIZE_FILE"
     exit 0
-  else
-    if (( current_size == 0 )); then
-      # remaining at 0 is fine
-      exit 0
-    else
-      # size remaining at a non-zero value longer than $THRESHOLD is NOT okay
-      now=$(date +'%s')
-      last_change=$(stat -c'%Y' "$SIZE_FILE")
-      elapsed=$((now - last_change))
-      if (( elapsed > THRESHOLD )); then
-        error="Queue ${QUEUE} has been stuck at ${last_size} for ${elapsed} seconds (threshold is ${THRESHOLD} seconds)"
-        # send the error message to both the pod log (file descriptor 1 of pid 1)...
-        echo "$error" > /proc/1/fd/1
-        # ...and the liveness probe failure message (STDERR)
-        echo "$error" >&2
-        exit 1
-      fi
-    fi
-  fi
 fi
+
+last_size=$(cat "$SIZE_FILE")
+now=$(date +'%s')
+last_change=$(stat -c'%Y' "$SIZE_FILE")
+elapsed=$((now - last_change))
+
+# exit early when the queue is empty
+if (( current_size == 0 )); then
+    log info "queue=\"${QUEUE}\" msg=\"queue is empty\" elapsed=\"${elapsed}\" last_size=\"${last_size}\" current_size=\"${current_size}\""
+    exit 0
+fi
+
+# update state and exit early when the queue size is changing
+if (( current_size != last_size )); then
+    log info "queue=\"${QUEUE}\" msg=\"queue is changing, an updated size will be recorded\" elapsed=\"${elapsed}\" last_size=\"${last_size}\" current_size=\"${current_size}\""
+    # only when it is changed do we update the size file (to preserve the timestamp of the change)
+    echo "$current_size" > "$SIZE_FILE"
+    exit 0
+fi
+
+# size remaining at a non-zero value longer than $THRESHOLD is NOT okay
+if (( elapsed > THRESHOLD )); then
+    log error "queue=\"${QUEUE}\" msg=\"queue has been stuck for greater than threshold ${THRESHOLD} seconds\" elapsed=\"${elapsed}\" last_size=\"${last_size:-n/a}\" current_size=\"${current_size}\""
+    exit 1
+fi
+
+# if we get here, the queue is not changing but it hasn't exceeded the threshold yet
+log warning "queue=\"${QUEUE}\" msg=\"queue is not changing, but threshold of ${THRESHOLD} seconds has not yet been exceeded\" elapsed=\"${elapsed}\" last_size=\"${last_size:-n/a}\" current_size=\"${current_size}\""
